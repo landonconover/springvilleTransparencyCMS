024a51686d649658c69ba848dae4cd6f46acc456
Landon Conover <landon.conover@gmail.com>
2024-11-03 15:07:29 -0700

    Metadata edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Springville Transparency`,
     author: {
       name: `Kyle Mathews`,
       summary: `who lives and works in San Francisco building useful things.`,
