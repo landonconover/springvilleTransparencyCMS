8a0d28663786d84b24856c350c5d56d055dbefa0
Landon Conover <landon.conover@gmail.com>
2024-11-16 20:27:05 -0700

    Some sort of Menu working

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,52 +3,51 @@ import { useState } from "react"
 import { Link, Script } from "gatsby"
 import "../sass/main.scss"
 
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+const drawerWidth = 288;
+
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let header
 
-  const [loaded, setLoaded] = useState(false)
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [isClosing, setIsClosing] = React.useState(false);
 
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
+  const handleDrawerClose = () => {
+    setIsClosing(true);
+    setMobileOpen(false);
+  };
 
-  return (
-    <div id="wrapper">
+  const handleDrawerTransitionEnd = () => {
+    setIsClosing(false);
+  };
 
-				{/* <!-- Main --> */}
-					<div id="main">
-						<div className="inner">
+  const handleDrawerToggle = () => {
+    if (!isClosing) {
+      setMobileOpen(!mobileOpen);
+    }
+  };
 
-							{/* <!-- Header --> */}
-								<header id="header">
-									<a href="index.html" className="logo"><strong>Springville Transparency</strong></a>
-									<ul className="icons">
-										<li><a href="#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
-										<li><a href="#" className="icon brands fa-facebook-f"><span className="label">Facebook</span></a></li>
-									</ul>
-								</header>
-
-                {children}
-
-
-
-						</div>
-					</div>
-
-				{/* <!-- Sidebar --> */}
-					<div id="sidebar">
+  const drawer = (
+    <div id="sidebar">
 						<div className="inner">
 
 							
@@ -89,17 +88,141 @@ const Layout = ({ location, title, children }) => {
 
 						</div>
 					</div>
+  );
 
-          <Script strategy="idle" src="/js/jquery.min.js" onLoad={() => setLoaded(true)}></Script>
-          <Script strategy="idle" src="/js/browser.min.js"></Script>
-          <Script strategy="idle" src="/js/breakpoints.min.js"></Script>
-          {loaded && <Script strategy="idle" src="/js/util.js" />}
-          {loaded && <Script strategy="idle" src="/js/main.js" />}
+//   if (isRootPath) {
+//     header = (
+//       <h1 className="main-heading">
+//         <Link to="/">{title}</Link>
+//       </h1>
+//     )
+//   } else {
+//     header = (
+//       <Link className="header-link-home" to="/">
+//         {title}
+//       </Link>
+//     )
+//   }
+
+  return (
+    <div id="wrapper">
+
+			{/* <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          > */}
+            
+          
+
+		  
+		  {/* </IconButton> */}
+
+				{/* <!-- Main --> */}
+					<div id="main">
+            <MenuIcon onClick={handleDrawerToggle}
+             sx={{ mr: 2, mt: 2, display: { xs: 'block', md: 'block', lg: 'none' } }} 
+             
+             />
+						<div className="inner">
+						
+							{/* <!-- Header --> */}
+								<header id="header">
+									<a href="index.html" className="logo"><strong>Springville Transparency</strong></a>
+									{/* <ul className="icons">
+										<li><a href="#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
+										<li><a href="#" className="icon brands fa-facebook-f"><span className="label">Facebook</span></a></li>
+									</ul> */}
+								</header>
+
+                {children}
+
+
+
+						</div>
+					</div>
+
+					
+				{/* <!-- Sidebar --> */}
+
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          
+          variant="temporary"
+          open={mobileOpen}
+          onTransitionEnd={handleDrawerTransitionEnd}
+          onClose={handleDrawerClose}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', md: 'block', lg: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', },
+
+          }}
+        >
+          {drawer}
+        </Drawer>
+		
+        {/* <Drawer
+          variant="permanent"
+          sx={(theme) => ({
+            position: 'static',
+            display: { xs: 'none', sm: 'block' }
+          })}
+
+          open
+        >
+          {drawer}
+        </Drawer> */}
+
+        <Box
+        sx={{
+            display: { xs: 'none', md: 'none', lg: 'block' },
+          }}
+        >
+          {drawer}
+        </Box>
+
+
+				
+					
+
           
           
 
 			</div>
   )
 }
+
+
+
+
+// function ResponsiveDrawer(props) {
+//   const { window } = props;
+  
+
+//   return (
+//     <Box sx={{ display: 'flex' }}>
+
+//       <AppBar
+//         position="fixed"
+//         sx={{
+//           width: { sm: `calc(100% - ${drawerWidth}px)` },
+//           ml: { sm: `${drawerWidth}px` },
+//         }}
+//       >
+//         <Toolbar>
+
+//         </Toolbar>
+//       </AppBar>
+      
+
+//     </Box>
+//   );
+// }
+
 
 export default Layout
